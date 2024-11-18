9627b3762c0e872898a2a28f9bbf0ecdb751c53d
Hemanth Bavirisetty <manikantahemanth5177@gmail.com>
2024-11-18 10:20:29 +0530

    1

---
+++ TaskManagementSystem/Frontend/tms/src/components/elements/GlobeDemo.jsx
@@ -0,0 +1,11 @@
+import Globe from "@/components/ui/globe";
+
+export function GlobeDemo() {
+    return (
+        <div className="">
+           
+            <Globe className="top-28" />
+            <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_200%,rgba(0,0,0,0.2),rgba(255,255,255,0))]" />
+        </div>
+    );
+}
